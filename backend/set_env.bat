d794a4c2bed125eaf8b5e7a62bfc2e9cbe009e1e
sherfyehia <109470170+sherfyehia@users.noreply.github.com>
2022-12-18 00:08:10 +0200

    new commint for screen error

---
+++ backend/set_env.bat
@@ -0,0 +1,11 @@
+set NODE_ENV=production
+set VERSION=1
+set TYPEORM_CONNECTION=postgres
+set TYPEORM_MIGRATIONS_DIR=./migrations
+set TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
+set TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
+set TYPEORM_HOST=localhost
+set TYPEORM_PORT=5532
+set TYPEORM_USERNAME=postgres
+set TYPEORM_PASSWORD=password
+set TYPEORM_DATABASE=glee
